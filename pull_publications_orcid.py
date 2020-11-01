dd7def4ec378ba80ccaf570899b10c4745cb54b3
Jan Povala <jan.povala11@imperial.ac.uk>
2020-11-01 00:06:19 +0000

    Puttting env stuff back in and addign pandoc to the requirements file.

--- pull_publications_orcid.py
+++ pull_publications_orcid.py
@@ -1,6 +1,7 @@
 import orcid
 import os
 from requests import RequestException
+from dotenv import load_dotenv
 import requests
 import pybtex
 import pypandoc
@@ -17,6 +18,8 @@ def doi2bib(doi):
     return r.text
 
 if __name__ == '__main__':
+    load_dotenv()
+
     orcid_ids = [
         "0000-0002-5112-7373",  # Jan
         "0000-0002-7503-2117",  # Ioannis
