67592d91930ab2c879786e62581d45b9940a90d2
Jan Povala <jan.povala11@imperial.ac.uk>
2020-11-02 11:59:47 +0000

    Fixing some issues with the ORCID updater.

--- _pages/publications.md
+++ _pages/publications.md
@@ -15,8 +15,45 @@ Statistics)*, 69(5):1067--1090, aug 2020. URL:
 <https://doi.org/10.1111%2Frssc.12431>,
 [doi:10.1111/rssc.12431](https://doi.org/10.1111/rssc.12431).
 
+Olga Mikheeva, Ieva Kazlauskaite, Hedvig Kjellström, and Carl Henrik Ek.
+Bayesian nonparametric shared multi-sequence time series segmentation.
+*arXiv preprint arXiv:2001.09886*, 2020.
+
+Ivan Ustyuzhaninov, Ieva Kazlauskaite, Markus Kaiser, Erik Bodin, Neill
+Campbell, and Carl Henrik Ek. Compositional uncertainty in deep gaussian
+processes. In *Conference on Uncertainty in Artificial Intelligence*,
+480--489. PMLR, 2020.
+
+Ivan Ustyuzhaninov, Ieva Kazlauskaite, Carl Henrik Ek, and Neill DF
+Campbell. Monotonic gaussian process flow. In *International Conference
+on Artificial Intelligence and Statistics (AISTATS 2020)*. arXiv
+preprint arXiv:1905.12930, 2020.
+
 Gishan D. Ranasinghe, Tony Lindgren, Mark Girolami, and Ajith K.
 Parlikad. A methodology for prognostics under the conditions of limited
 failure data availability. *IEEE Access*, 7:183996--184007, 2019. URL:
 <https://doi.org/10.1109%2Faccess.2019.2960310>,
 [doi:10.1109/access.2019.2960310](https://doi.org/10.1109/access.2019.2960310).
+
+Ieva Kazlauskaite, Carl Henrik Ek, and Neill DF Campbell. Gaussian
+process latent variable alignment learning. In *International Conference
+on Artificial Intelligence and Statistics (AISTATS 2019)*. Proceedings
+of Machine Learning Research, 2019.
+
+Erik Bodin, Markus Kaiser, Ieva Kazlauskaite, Neill D. F. Campbell, and
+Carl Henrik Ek. Modulated bayesian optimization using latent gaussian
+process models. 2019.
+
+Erik Bodin, Markus Kaiser, Ieva Kazlauskaite, Zhenwen Dai, Neill DF
+Campbell, and Carl Henrik Ek. Modulating surrogates for bayesian
+optimization. In *International Conference on Machine Learning (ICML
+2020)*, arXiv--1906. 2019.
+
+Ieva Kazlauskaite, Ivan Ustyuzhaninov, Carl Henrik Ek, and Neill DF
+Campbell. Sequence alignment with dirichlet process mixtures. In
+*NeurIPS, Workshop on Bayesian Nonparametrics (BNP\@NeurIPS), 2018*.
+arXiv preprint arXiv:1811.10689, 2018.
+
+Ieva Kazlauskaite, Carl Henrik Ek, and Neill Campbell. Learning
+alignments from latent space structures. In *NIPS Workshop on Learning
+in High Dimensions with Structure*. University of Bath, 2016.
--- pull_publications_orcid.py
+++ pull_publications_orcid.py
@@ -1,3 +1,5 @@
+import logging
+
 import orcid
 import os
 from requests import RequestException
@@ -9,19 +11,37 @@ import re
 import random
 import string
 import toolz
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
+import logging
 
 
 def doi2bib(doi):
-    #
+    retry_strategy = Retry(
+        total=3,
+        status_forcelist=[429, 500, 502, 503, 504],
+        method_whitelist=["HEAD", "GET", "OPTIONS"]
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    http = requests.Session()
+    http.mount("https://", adapter)
+    http.mount("http://", adapter)
+
     url = f"http://dx.doi.org/{doi}"
     headers = {'Accept': 'application/x-bibtex'}
-    r = requests.get(url, headers=headers)
+    logging.info(f"Retrieving bibtex from {url}")
+    r = http.get(url, headers=headers)
+
+    # r = requests.get(url, headers=headers)
     if r.status_code is not 200:
+        logging.info(r)
         raise RequestException("Failed to retrieve BIB for a DOI.")
     return r.text
 
 if __name__ == '__main__':
     load_dotenv()
+    logging.basicConfig(level=logging.INFO)
 
     orcid_ids = [
         "0000-0002-5112-7373",  # Jan
@@ -32,52 +52,66 @@ if __name__ == '__main__':
     api = orcid.PublicAPI(os.environ['ORCID_CLIENT_ID'], os.environ['ORCID_CLIENT_SECRET'])
     search_token = api.get_search_token_from_orcid()
 
-    bibtex_list = []
+    pub_list = []
     for orcid_id in orcid_ids:
-        print(f"Downloading publications for ORCID ID: {orcid_id}.")
+        logging.info(f"Downloading publications for ORCID ID: {orcid_id}.")
         query_result = api.read_record_public(orcid_id, 'works', search_token)
         for orcid_entry in query_result['group']:
 
             put_code = orcid_entry['work-summary'][0]['put-code']
+
+            orcid_full_entry = api.read_record_public(orcid_id, 'work', search_token, put_code=put_code)
+            logging.info(orcid_full_entry)
+
             try:
-                year = orcid_entry['work-summary'][0]['publication-date']['year']['value']
+                year = orcid_full_entry['publication-date']['year']['value']
             except KeyError:
                 year = None
 
             entry_doi = None
             try:
-                external_ids = orcid_entry['external-ids']['external-id']
+                external_ids = orcid_full_entry['external-ids']['external-id']
                 for external_id in external_ids:
                     if external_id["external-id-type"] == "doi":
                         entry_doi = external_id["external-id-value"]
-            except KeyError:
-                print("Unable to extract DOI")
-
+            except (KeyError, TypeError):
+                logging.info("Unable to extract DOI.")
             try:
-                orcid_bib_style = orcid_entry['citation']['citation-type'].lower()
+                orcid_bib_style = orcid_full_entry['citation']['citation-type'].lower()
                 if orcid_bib_style == 'bibtex':
-                    bibtex_list.append((orcid_id, put_code, year, entry_doi, orcid_entry['citation']['citation-value']))
+                    pub_list.append((orcid_id, put_code, year, entry_doi, orcid_full_entry['citation']['citation-value']))
                 # key exists in dict
-            except KeyError:
-                # try with the DOI lookup
+            except (KeyError, TypeError):
+                logging.info("No bibtex available, trying DOI lookup.")
                 try:
                     if entry_doi is not None:
-                        bibtex_list.append((orcid_id, put_code, year, entry_doi, doi2bib(entry_doi)))
+                        pub_list.append((orcid_id, put_code, year, entry_doi, doi2bib(entry_doi)))
                 except RequestException as e:
-                    # build an entry manually
-                    print(f"Problem retrieving BIB details for: {orcid_entry}")
-                    print(e)
+                    logging.info(f"Problem retrieving BIB details for: {orcid_entry}")
+                    logging.info(e)
                     continue
 
-    bibtex_list = list(toolz.unique(bibtex_list, key=lambda x : x[1])) # filter duplicates based on put code
-    bibtex_list = list(toolz.unique(bibtex_list, key=lambda x: x[3]))  # filter duplicates based on DOI
-    bibtex_list = sorted(bibtex_list, key=lambda x: x[2], reverse=True)
+    # Remove duplicated
+    attributes_to_check_uniqueness = [
+        1, # put codes
+        3,
+    ]
+    for key in attributes_to_check_uniqueness:
+        filtered_pub_list = []
+        unique_values = set()
+        for pub_entry in pub_list:
+            if (pub_entry[key] is None) or (not pub_entry[key] in unique_values):
+                filtered_pub_list.append(pub_entry)
+                if pub_entry[key] is not None:
+                    unique_values.add(pub_entry[key])
+        pub_list = filtered_pub_list
+
+    pub_list = sorted(pub_list, key=lambda x: x[2], reverse=True)
 
     list_bibtex_strings = []
-    for _, _, _, _, bib_str in bibtex_list:
-
+    for _, _, _, _, bib_str in pub_list:
         # add random string token to the bib id to prevent any clashes as bibtex is generated from multiple sources
-        parsed_bib_re = re.compile(r"^(@[a-z]+\{)([^\,]+)((.*\n.+)+)", re.MULTILINE)
+        parsed_bib_re = re.compile(r"^(@[a-z]+\{)([^\,]+)((.*\n.*)*)", re.MULTILINE)
         matched_str = parsed_bib_re.match(bib_str)
 
         random_token = ''.join(random.choice(string.ascii_letters) for i in range(10))
@@ -94,7 +128,7 @@ if __name__ == '__main__':
             formatted_md_entry = formatted_md_entry.split("\n", 1)[1]
             publications_md_content += formatted_md_entry
         except Exception:
-            print(f"Failing to format: {bibtex_string}")
+            logging.info(f"Failing to format: {bibtex_string}")
             continue
 
 
